04f3b804b70f9c299c59c6d9f7207ab95bf6407b
kentr0w <qbextted0@gmail.com>
2024-07-08 21:46:35 +0200

    Updated

---
+++ examples/auto-generation/test-tree.py
@@ -0,0 +1,40 @@
+from kernel_tuner import tune_kernel
+from kernel_tuner.interface import auto_tune_kernel
+from kernel_tuner.utils.directives import (
+    DirectiveCode,
+    OpenMP,
+    Cxx,
+)
+
+code = """
+#include <stdlib.h>
+#include <omp.h>
+
+#define VECTOR_SIZE 10000
+
+void vector_add(float *a, float *b, float *c) {
+	#pragma tuner start vector_add a(float*:VECTOR_SIZE) b(float*:VECTOR_SIZE) c(float*:VECTOR_SIZE) size(int:VECTOR_SIZE)
+	#pragma omp target parallel num_threads(nthreads)
+  #pragma omp for schedule(static)
+	for (int i = 0; i < VECTOR_SIZE; i++ ) {
+		c[i] = a[i] + b[i];
+	}
+	#pragma tuner stop
+}
+"""
+
+# Extract tunable directive
+directive = DirectiveCode(OpenMP(), Cxx())
+
+tune_params = dict()
+tune_params["nthreads"] = [4, 8]
+
+auto_tune_kernel(
+    "vector_add",
+    code,
+    0,
+    tune_params=tune_params,
+    compiler_options=["-fopenmp", "-mp=gpu"],
+    compiler="nvc++",
+    directive=directive
+)
